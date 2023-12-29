ef7cea837ea7dc1c071b88644eaf8bd55aa8819c
KennyTang <152666563+CUHK-TCSS1089@users.noreply.github.com>
2023-12-29 10:31:01 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,104 @@
+# importing necessary libraries
 from exif import Image
 from datetime import datetime
-import cv2
-import math
 from pathlib import Path
-from math import sin, cos, sqrt, atan2, radians
+from math import sin, cos, sqrt, radians, asin
 import pandas as pd
-import joblib
-from time import sleep
 from picamera import PiCamera
+from sense_hat import SenseHat
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+import numpy as np
+# loading training data from csv to dataframe
+data = pd.read_csv('speeds1.csv')
+df = pd.DataFrame(data)
 
-def convert_to_cv(image_1, image_2):
-    image_1_cv = cv2.imread(image_1, 0)
-    image_2_cv = cv2.imread(image_2, 0)
-    return image_1_cv, image_2_cv
 
-def calculate_features(image_1_cv, image_2_cv, feature_number):
-    orb = cv2.ORB_create(nfeatures = feature_number)
-    keypoints_1, descriptors_1 = orb.detectAndCompute(image_1_cv, None)
-    keypoints_2, descriptors_2 = orb.detectAndCompute(image_2_cv, None)
-    return keypoints_1, keypoints_2, descriptors_1, descriptors_2
+accels = []
+time = []
+long =[]
+lati = []
+pyths=[]
+distances =[]
+aas =[]
+# Calculating acceleration, time, distance, etc. for training model
+for i in range(len(df)-1):
+    # Calculating acceleration
+    accel = (df['speed'][i] - df['speed'][i+1])/(df['diff_time'][i])
+    
+    # Calculating latitude and longitude differences
+    lat = df['lat'][i] - df['lat_1'][i]
+    lon = df['long'][i] - df['long_1'][i]
+    
+    # Converting latitude and longitude differences to radians for further calculations
+    dlat = radians(lat)
+    dlon = radians(lon)
+    
+    # Calculating the Pythagorean distance between latitude and longitude differences
+    pyth = np.sqrt(lat**2 + lon**2)
+    
+    # Calculating distance using haversine formula
+    a = sin(dlat/2)**2 + cos(radians(df['lat'][i])) * cos(radians(df['lat_1'][i])) *sin(dlon/2)**2#In our project, becuase unknow the exactly distance between earth to ISS , we choose this put in ML because it do not use the distance between earth to ISS 
+    distance=2*asin(sqrt(a))*(6371+420)*1000    
+    # Appending the calculated values to respective lists
+    accels.append(accel)
+    time.append(df['diff_time'][i])
+    long.append(lon)
+    lati.append(lat)
+    pyths.append(pyth)
+    distances.append(distance)
+    aas.append(a)
+    distances.append(distance)
+    
+# Creating dataframes for calculated values
+adf = pd.DataFrame(accels)
+adf.rename(columns={0:'accels'},inplace=True)
+tdf = pd.DataFrame(time)
+tdf.rename(columns={0:'time'},inplace=True)
+latit = pd.DataFrame(lati)
+latit.rename(columns={0:'latit'},inplace=True)
+longi = pd.DataFrame(long)
+longi.rename(columns={0:'longi'},inplace=True)
+pyt = pd.DataFrame(pyths)
+pyt.rename(columns={0:'pyth'},inplace=True)
+distanc = pd.DataFrame(distances)
+distanc.rename(columns={0:'distance'},inplace=True)
+asss = pd.DataFrame(aas)
+asss.rename(columns={0:'a'},inplace=True)
 
-def calculate_matches(descriptors_1, descriptors_2):
-    brute_force = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = brute_force.match(descriptors_1, descriptors_2)
-    matches = sorted(matches, key=lambda x: x.distance)
-    return matches
+# Modeling Polynomial Regression
+class PolynomialRegressionModel:
+    def __init__(self, degree=2):
+        self.degree = degree
+        self.poly = PolynomialFeatures(degree=degree)
+        self.model = LinearRegression()
 
+    def fit(self, features, labels):
+        features_poly = self.poly.fit_transform(features)
+        self.model.fit(features_poly, labels)
+
+    def predict(self, features):
+        features_poly = self.poly.transform(features)
+        return self.model.predict(features_poly)
+
+    def evaluate(self, features, labels):
+        features_poly = self.poly.transform(features)
+        predictions = self.model.predict(features_poly)
+        mse = mean_squared_error(labels, predictions)
+        return mse
+    
+# creating features and labels for the model
+fe = adf.join(pd.DataFrame(df['lat']))
+fe = fe.join(asss)
+#fe = adf.join(tdf)
+features = fe
+labels = df['speed'][:len(df)-1]
+# fitting the model
+model = PolynomialRegressionModel(degree=5)
+model.fit(features, labels)
+
+# functions for handling image data
 def get_time(image):
     with open(image, 'rb') as image_file:
         img = Image(image_file)
@@ -65,7 +138,7 @@ def degree_to_decimal_lon(longitude):
     return longitude_decimal
 
 
-def calculate_speed(image_1, image_2):
+def calculate_a(image_1, image_2):#Haversine formula
     latitude1 = get_latitude(image_1)
     longitude1 = get_longitude(image_1)
     latitude2 = get_latitude(image_2)
@@ -74,95 +147,56 @@ def calculate_speed(image_1, image_2):
     lon_decimal1 = degree_to_decimal_lon(longitude1)
     lat_decimal2 = degree_to_decimal_lat(latitude2)
     lon_decimal2 = degree_to_decimal_lon(longitude2)
-    R = 6371 + 420
-
-    a = radians(lat_decimal2 - lat_decimal1)
-    b = radians(lon_decimal2 - lon_decimal1)
-    x = sin(a/2)**2 + cos(a) * cos(a) * sin(b/2)**2
-    c = 2 * atan2(sqrt(x), sqrt(1-x))
-
-    distance = R * c
+    dlat = radians(lat_decimal2 - lat_decimal1)
+    dlon = radians(lon_decimal2 - lon_decimal1)
+    a = sin(dlat / 2) ** 2 + cos(radians(lat_decimal1)) * cos(radians(lat_decimal1)) * sin(dlon / 2) ** 2#In our project, becuase unknow the exactly distance between earth to ISS , we choose this no the full formula because it do not use the distance between earth to ISS 
     time_difference = get_time_difference(image_1, image_2)
-    speed = distance / time_difference
-    return speed
+    return a
+
+def calculate_accel():
+    sense = SenseHat()
+    sense.clear()
+    acceleration = sense.get_accelerometer_raw()
+    x = acceleration['x']
+    y = acceleration['y']
+    z = acceleration['z']
+    accel = sqrt(x**2 + y**2 + z**2)# Fing vector acceleration
+    return accel
 
 
-def predict_speed(image):
-    model = joblib.load('model.pkl')
-    latitude = get_latitude(image)
-    longitude = get_longitude(image)
+
+def predict_speed(image_1,image_2):
+    latitude = get_latitude(image_1)
     lat_decimal = degree_to_decimal_lat(latitude)
-    lon_decimal = degree_to_decimal_lon(longitude)
-    predicted_speed = model.predict([[lat_decimal, lon_decimal]])  
+    predicted_speed = model.predict([[calculate_accel(), lat_decimal, calculate_a(image_1, image_2)]])#ML prediction
     return predicted_speed
 
-def find_matching_coordinates(keypoints_1, keypoints_2, matches):
-    coordinates_1 = []
-    coordinates_2 = []
-    for match in matches:
-        image_1_idx = match.queryIdx
-        image_2_idx = match.trainIdx
-        (x1,y1) = keypoints_1[image_1_idx].pt
-        (x2,y2) = keypoints_2[image_2_idx].pt
-        coordinates_1.append((x1,y1))
-        coordinates_2.append((x2,y2))
-    return coordinates_1, coordinates_2
-
-def calculate_mean_distance(coordinates_1, coordinates_2):
-    all_distances = 0
-    merged_coordinates = list(zip(coordinates_1, coordinates_2))
-    for coordinate in merged_coordinates:
-        x_difference = coordinate[0][0] - coordinate[1][0]
-        y_difference = coordinate[0][1] - coordinate[1][1]
-        distance = math.hypot(x_difference, y_difference)
-        all_distances = all_distances + distance
-    return all_distances / len(merged_coordinates)
-
-def calculate_speed_in_kmps(feature_distance, GSD, time_difference):
-    distance = feature_distance * GSD / 100000
-    speed = distance / time_difference
-    return speed
-
-
+# main function to calculate average speed
 def calculate_average_speed():
-    speeds = []
-    for i in range(1, 42):
-        image = f'image{i}.jpg'
-        speed = predict_speed(image)
-        speeds.append(speed)
-    average_speed_prediction = sum(speeds) / len(speeds) / 1000
-    print("===================================================")
-    print("Average Speed (Machine Learning):", average_speed_prediction)
-    
     speeds = []
     for i in range(1, 42):
         image_1 = f'image{i}.jpg'
         image_2 = f'image{i+1}.jpg'
-        speed = calculate_speed(image_1, image_2)
+        speed = predict_speed(image_1,image_2)
         speeds.append(speed)
-    average_speed_1_2 = sum(speeds) / len(speeds)
-    print("Average Speed (Haversine Formula):", average_speed_1_2)
+    average_speed_prediction = sum(speeds) / len(speeds) / 1000
+    print("===================================================")
+    print("Average Speed (Machine Learning):", average_speed_prediction)
 
-    image_1 = 'image12.jpg'
-    image_2 = 'image13.jpg'
-    time_difference = get_time_difference(image_1, image_2)
-    image_1_cv, image_2_cv = convert_to_cv(image_1, image_2)
-    keypoints_1, keypoints_2, descriptors_1, descriptors_2 = calculate_features(image_1_cv, image_2_cv, 1000)
-    matches = calculate_matches(descriptors_1, descriptors_2)
-    coordinates_1, coordinates_2 = find_matching_coordinates(keypoints_1, keypoints_2, matches)
-    average_feature_distance = calculate_mean_distance(coordinates_1, coordinates_2)
-    average_speed_feature_matching = calculate_speed_in_kmps(average_feature_distance, 12648, time_difference)
-    print("Average Speed (OpenCV Feature Matching):", average_speed_feature_matching)
 
-    # Calculate weighted average speed
-    total_average_speed = (0.1 * average_speed_1_2) + (0.75* average_speed_prediction) + (0.15 * average_speed_feature_matching)
-
-    return total_average_speed
+    return average_speed_prediction
+# main execution
 if __name__ == "__main__":
+    for i in range(1, 42):
+        camera = PiCamera()
+        camera.resolution = (1, 1)
+        base_folder = Path(__file__).parent.resolve()
+        print('got')
     total_average_speed = calculate_average_speed()
     total = []
     total.append(total_average_speed)
     total_average_speed_str = str(total_average_speed)
+    # writing results to a file
     with open('result.txt', 'x') as Doc:
         Doc.write(total_average_speed_str)
         print('Total Average Speed:', + total_average_speed)
